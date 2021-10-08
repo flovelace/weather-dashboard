9d76303752b7915ad28404fcceb78f4987c32fe0
fairfax <lovelace.af@gmail.com>
2021-10-08 00:06:51 -0400

    managed to pull api and append the requested data to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,43 +25,61 @@
 var apiKey = 'f9aba52981da94981ae6d22cd818ec62'; //APIKey
 var cityNameArray = []; //empty array for local storage
 
-var longLat = function(cityName) {
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-    //fetch the long/lat
-    fetch(weatherUrl) //handling the promise
-    .then(function(response) {
-        console.log("received info back")
-        return response.json(); 
+//search city function
+function search(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=f9aba52981da94981ae6d22cd818ec62`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // add the name of the city
+        document.getElementById('current-city').innerHTML = data.name;
+        // add the temperature
+        document.getElementById('celcius').innerHTML = Math.floor(data.main.temp);
+        // add the humidity
+        document.getElementById('humidity').innerHTML = data.main.humidity;
+        // add the wind
+        document.getElementById('wnd').innerHTML = data.wind.speed
+
+        let lon = data.coord.lon;
+        let Lat = data.coord.Lat;
     })
 }
 
+// submit button
+document.getElementById('user-form').addEventListener("submit", function (e) {
+    e.preventDefault();
+    let userInput = document.getElementById("city-name");
+    search("glasgow");
+})
+
 
 // var getWeatherForecast = function(cityName, lon, lat) {
-var getWeatherForecast = function(cityName) {
-    console.log("function was called");
+//var getWeatherForecast = function(cityName) {
+    //console.log("function was called");
 
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    //var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
 
-    fetch(url)
+    //fetch(url)
         // Handeling the Promise 
-        .then(function(response) {
-            console.log("Recieved Info Back...")
-            return response.json();
-        })
-        .then(function(data) {
-            console.log("Data ...");
+        //.then(function(response) {
+            //console.log("Recieved Info Back...")
+            //return response.json();
+        //})
+        //.then(function(data) {
+            //console.log("Data ...");
             // Here we are getting actual DATA back 
-            console.log(data);
+            //console.log(data);
 
             // Parse the returned Data
-            console.log(`City Name: ${data.name}`);
-            console.log(`Lat: ${data.coord.lat}`);
-            console.log(`Current Temp: ${data.main.temp}`);
-        })
+            //console.log(`City Name: ${data.name}`);
+            //console.log(`Lat: ${data.coord.lat}`);
+            //console.log(`Current Temp: ${data.main.temp}`);
+        //})
 
-    console.log("I'm after the Fetch Method");
+    //console.log("I'm after the Fetch Method");
 
-};
+//};
 
-getWeatherForecast("Austin");
+//getWeatherForecast("Austin");
