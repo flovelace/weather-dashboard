ac67596a3566211ab65517b3bd1fb26585154374
fairfax <lovelace.af@gmail.com>
2021-10-08 22:20:32 -0400

    added the rest of the cards for 5 day/added second api call for uv index. wind not returning correct kmph

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,30 +27,72 @@ var cityNameArray = []; //empty array for local storage
 
 //search city function
 function search(cityName) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=f9aba52981da94981ae6d22cd818ec62`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        // add the name of the city
+        // append the name of the city
         document.getElementById('current-city').innerHTML = data.name;
-        // add the temperature
+        // append the temperature. Use math floor to round so we don't have decimals
         document.getElementById('celcius').innerHTML = Math.floor(data.main.temp);
-        // add the humidity
+        // append the humidity
         document.getElementById('humidity').innerHTML = data.main.humidity;
-        // add the wind
+        // append the wind
         document.getElementById('wnd').innerHTML = data.wind.speed
+        // append the weather icons
+        let weatherIcons = data.weather[0].icon;
+        document.getElementById('weatherIcon').setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcons}@2x.png`);
 
+        console.log(data);   // <-- Find the Weather Icon
+        // the lat and lon are required so we can make our second api call to OneCall
         let lon = data.coord.lon;
-        let Lat = data.coord.Lat;
+        let lat = data.coord.lat;
+        console.log(lat, lon);
+
+        // Now we have Lat and Long Values
+        // We have to make a second request (to another API address)
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+
+                console.log(data.current.uvi);
+                console.log(data.daily);
+
+                document.getElementById('uv-indx').innerHTML = data.current.uvi;
+            })
     })
 }
+
+
+function forecast(lat, lon) {
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+            console.log(data.current.uvi);
+            console.log(data.daily);
+        })
+}
+
 
 // submit button
 document.getElementById('user-form').addEventListener("submit", function (e) {
     e.preventDefault();
-    let userInput = document.getElementById("city-name");
-    search("glasgow");
+    // let userInputElement = document.getElementById("cityname");
+   // console.log(userInputElement);
+
+    let userInput = document.getElementById("cityname").value;
+    console.log(userInput);
+    search(userInput);
+
 })
 
 
